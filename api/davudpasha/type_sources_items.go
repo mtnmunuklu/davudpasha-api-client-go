08e4b8212a22b9524ae06c7ef2bf151ca7941b8e
metinmunuklu <mtnmunuklu@gmail.com>
2024-05-15 09:56:52 +0300

    Change file names

--- api/davudpasha/type_sources_items.go
+++ api/davudpasha/type_sources_items.go
@@ -45,3 +45,104 @@ type SourcesItems struct {
 	// AdditionalProperties stores any additional properties not explicitly defined in the struct.
 	AdditionalProperties map[string]interface{}
 }
+
+// NewSourcesItems creates a new SourcesItems object.
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed.
+func NewSourcesItems() *SourcesItems {
+	this := SourcesItems{}
+	return &this
+}
+
+// NewSourcesItemsWithDefaults creates a new SourcesItems object.
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set.
+func NewSourcesItemsWithDefault() *SourcesItems {
+	this := SourcesItems{}
+	return &this
+}
+
+// GetId returns the Id field value if set, zero value otherwise.
+func (o *SourcesItems) GetId() string {
+	if o == nil || o.Id == nil {
+		var ret string
+		return ret
+	}
+	return *o.Id
+}
+
+// GetIdOk returns a tuple with the Id field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SourcesItems) GetIdOk() (*string, bool) {
+	if o == nil || o.Id == nil {
+		return nil, false
+	}
+	return o.Id, true
+}
+
+// HasId returns a boolean if a field has been set.
+func (o *SourcesItems) HasId() bool {
+	return o != nil && o.Id != nil
+}
+
+// SetId gets a reference to the given string and assigns it to the Id field.
+func (o *SourcesItems) SetId(v string) {
+	o.Id = &v
+}
+
+// GetEnabled returns the Enabled field value if set, zero value otherwise.
+func (o *SourcesItems) GetEnabled() bool {
+	if o == nil || o.Enabled == nil {
+		var ret bool
+		return ret
+	}
+	return *o.Enabled
+}
+
+// GetEnabledOk returns a tuple with the Enabled field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SourcesItems) GetEnabledOk() (*bool, bool) {
+	if o == nil || o.Enabled == nil {
+		return nil, false
+	}
+	return o.Enabled, true
+}
+
+// HasEnabled returns a boolean if a field has been set.
+func (o *SourcesItems) HasEnabled() bool {
+	return o != nil && o.Enabled != nil
+}
+
+// SetEnabled gets a reference to the given bool and assigns it to the Enabled field.
+func (o *SourcesItems) SetEnabled(v bool) {
+	o.Enabled = &v
+}
+
+// GetName returns the Name field value if set, zero value otherwise.
+func (o *SourcesItems) GetName() string {
+	if o == nil || o.Name == nil {
+		var ret string
+		return ret
+	}
+	return *o.Name
+}
+
+// GetNameOk returns a tuple with the Name field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SourcesItems) GetNameOk() (*string, bool) {
+	if o == nil || o.Name == nil {
+		return nil, false
+	}
+	return o.Name, true
+}
+
+// HasName returns a boolean if a field has been set.
+func (o *SourcesItems) HasName() bool {
+	return o != nil && o.Name != nil
+}
+
+// SetName gets a reference to the given string and assigns it to the Name field.
+func (o *SourcesItems) SetName(v string) {
+	o.Name = &v
+}
