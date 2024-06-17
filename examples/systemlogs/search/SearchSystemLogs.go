0f2d0ee733f662dab17f0593f31d6061fdbc6091
mtnmunuklu <mtnmunuklu@gmail.com>
2024-06-17 15:22:16 +0300

    Add example for system logs api

---
+++ examples/systemlogs/search/SearchSystemLogs.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/mtnmunuklu/davudpasha-api-client-go/api/common"
+	"github.com/mtnmunuklu/davudpasha-api-client-go/api/davudpasha"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	body := davudpasha.SystemLogsSearchRequest{
+		AppName:   common.PtrString(""),
+		FromIndex: common.PtrInt64(0),
+		PageSize:  common.PtrInt64(0),
+		DateTimeRange: &davudpasha.DateTimeRange{
+			DateTimeType: davudpasha.DATETIMETYPE_LAST10MINUTES,
+		},
+		Filter:                  common.PtrString("keyword"),
+		ShowServicesData:        common.PtrBool(true),
+		ShowAllTenantsData:      common.PtrBool(true),
+		SmartRestRequestContext: common.PtrString("-<SmartRestRequestContext>-"),
+		VisualStyled:            common.PtrBool(true),
+	}
+	ctx := common.NewDefaultContext(context.Background())
+	//ctx := common.NewContextWithEnvParams(context.Background(), "DP_SITE", "DP_API_KEY")
+	configuration := common.NewConfiguration()
+	configuration.SetHTTPClientWithInsecureSkipVerify()
+	apiClient := common.NewAPIClient(configuration)
+	api := davudpasha.NewSystemLogsApi(apiClient)
+	resp, r, err := api.SearchSystemLogs(ctx, *davudpasha.NewSearchSystemLogsOptionalParameters().WithBody(body))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `SystemLogsApi.SearchSystemLogs`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", " ")
+	fmt.Fprintf(os.Stdout, "Response from `SystemLogsApi.SearchSystemLogs`:\n%s\n", responseContent)
+
+}
