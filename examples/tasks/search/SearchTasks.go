3f308bec1dd292a10a2b1294fc86b6d1b3532e77
mtnmunuklu <mtnmunuklu@gmail.com>
2024-06-18 16:20:55 +0300

    Add example for search tasks

---
+++ examples/tasks/search/SearchTasks.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/mtnmunuklu/davudpasha-api-client-go/api/common"
+	"github.com/mtnmunuklu/davudpasha-api-client-go/api/davudpasha"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	body := davudpasha.TasksSearchRequest{
+		Filter:                  common.PtrString("task-name"),
+		ExtraFilter:             common.PtrString(""),
+		ShowServiceData:         common.PtrBool(true),
+		SmartRestRequestContext: common.PtrString("-<SmartRestRequestContext>-"),
+	}
+	ctx := common.NewDefaultContext(context.Background())
+	//ctx := common.NewContextWithEnvParams(context.Background(), "DP_SITE", "DP_API_KEY")
+	configuration := common.NewConfiguration()
+	configuration.SetHTTPClientWithInsecureSkipVerify()
+	apiClient := common.NewAPIClient(configuration)
+	api := davudpasha.NewTasksApi(apiClient)
+	resp, r, err := api.SearchTasks(ctx, *davudpasha.NewSearchTasksOptionalParameters().WithBody(body))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `TasksApi.SearchTasks`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", " ")
+	fmt.Fprintf(os.Stdout, "Response from `TasksApi.SearchTasks`:\n%s\n", responseContent)
+}
