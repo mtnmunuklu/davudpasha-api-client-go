4b1aefa378a2291dc0244f5667e100aed74fc823
mtnmunuklu <mtnmunuklu@gmail.com>
2024-06-22 20:07:08 +0300

    Add example for search classifications

---
+++ examples/classifications/search/SearchClassifications.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/mtnmunuklu/davudpasha-api-client-go/api/common"
+	"github.com/mtnmunuklu/davudpasha-api-client-go/api/davudpasha"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	body := davudpasha.ClassificationsSearchRequest{
+		Filter:                  common.PtrString("test-classification"),
+		SmartRestRequestContext: common.PtrString("-<SmartRestRequestContext>-"),
+	}
+	ctx := common.NewDefaultContext(context.Background())
+	//ctx := common.NewContextWithEnvParams(context.Background(), "DP_SITE", "DP_API_KEY")
+	configuration := common.NewConfiguration()
+	configuration.SetHTTPClientWithInsecureSkipVerify()
+	apiClient := common.NewAPIClient(configuration)
+	api := davudpasha.NewClassificationsApi(apiClient)
+	resp, r, err := api.SearchClassifications(ctx, *davudpasha.NewSearchClassificationsOptionalParameters().WithBody(body))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `ClassificationsApi.SearchClassifications`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", " ")
+	fmt.Fprintf(os.Stdout, "Response from `ClassificationsApi.SearchClassifications`:\n%s\n", responseContent)
+
+}
