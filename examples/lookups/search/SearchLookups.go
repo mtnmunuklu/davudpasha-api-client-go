d9c733d7c5a8e2b13075ba1b63548d635c7f6c58
mtnmunuklu <mtnmunuklu@gmail.com>
2024-07-14 19:05:45 +0300

    Add example for search lookups

---
+++ examples/lookups/search/SearchLookups.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/mtnmunuklu/davudpasha-api-client-go/api/common"
+	"github.com/mtnmunuklu/davudpasha-api-client-go/api/davudpasha"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	body := davudpasha.LookupsSearchRequest{
+		Filter:                  common.PtrString("test-lookup"),
+		SmartRestRequestContext: common.PtrString("-<SmartRestRequestContext>-"),
+	}
+	ctx := common.NewDefaultContext(context.Background())
+	//ctx := common.NewContextWithEnvParams(context.Background(), "DP_SITE", "DP_API_KEY")
+	configuration := common.NewConfiguration()
+	configuration.SetHTTPClientWithInsecureSkipVerify()
+	apiClient := common.NewAPIClient(configuration)
+	api := davudpasha.NewLookupsApi(apiClient)
+	resp, r, err := api.SearchLookups(ctx, *davudpasha.NewSearchLookupsOptionalParameters().WithBody(body))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `LookupsApi.SearchLookups`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", " ")
+	fmt.Fprintf(os.Stdout, "Response from `LookupsApi.SearchLookups`:\n%s\n", responseContent)
+
+}
